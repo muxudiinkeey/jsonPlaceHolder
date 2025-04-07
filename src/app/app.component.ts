3b4616d79d487ea16db8a5284b1f398d5e4cfc14
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-04-07 08:10:50 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { Iuser } from './user-list/user';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserListComponent],
+  imports: [RouterOutlet, UserListComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'userList' },
+    { path: 'userDetails', component: UserDetailsComponent },
+    { path: 'userList', component: UserListComponent},
+    { path: 'userRegister', component: UserRegisterComponent},
+    { path: '**', component: PageNotFoundComponent},
+];
